c5a0fddccc713e46b335ebc39697111a3acc6907
error <error@error-unlimited.net>
2024-08-19 22:37:24 -0500

    0.1.3

--- src/Source/FlossiesMod.cs
+++ src/Source/FlossiesMod.cs
@@ -1,12 +1,20 @@
 using System.Reflection;
 using Verse;
 using HarmonyLib;
+using System;
 
 namespace FlossiesMod {
 	public class FlossiesMod: Mod {
+		public static FlossiesMod Instance { get; private set; }
+
+		public string Name => Content.ModMetaData.Name;
+		public string Version => Content.ModMetaData.ModVersion;
+
 		public FlossiesMod( ModContentPack content ): base( content ) {
+			Instance = this;
 			var harmony = new Harmony( "Error.FlossiesMod" );
 			harmony.PatchAll( Assembly.GetExecutingAssembly() );
+			Log.Message( $"{Name} v{Version} initialized" );
 		}
 	}
 }
--- src/Source/Patch_MainMenuDrawer.cs
+++ src/Source/Patch_MainMenuDrawer.cs
@@ -9,12 +9,47 @@ namespace FlossiesMod {
 	public static class Patch_MainMenuDrawer_Init {
 		public static void Postfix() {
 			try {
-				Log.Message( "Patching logo..." );
-				var texTitleField = AccessTools.Field( typeof(MainMenuDrawer), "TexTitle" );
-				texTitleField.SetValue( null, ContentFinder<Texture2D>.Get( "UI/HeroArt/GameTitle" ) );
-				var titleSize = AccessTools.Field( typeof(MainMenuDrawer), "TitleSize" );
-				titleSize.SetValue( null, new Vector2( 1032f, 163f ) );
-				Log.Message( "Done patching logo" );
+				TexTitleOverlay = ContentFinder<Texture2D>.Get( "UI/HeroArt/GameTitleOverlay" );
+			} catch( Exception ex ) {
+				Log.Error( ex.ToString() );
+			}
+		}
+
+		public static Texture2D TexTitleOverlay { get; private set; }
+	}
+
+	[HarmonyPatch( typeof(MainMenuDrawer), "MainMenuOnGUI" )]
+	public static class Patch_MainMenuDrawer_MainMenuOnGUI {
+		public static void Postfix() {
+			try {
+				var paneSize = new Vector2( 450f, 750f );
+				
+				Rect rect = new Rect( UI.screenWidth / 2f - paneSize.x / 2f, UI.screenHeight / 2f - paneSize.y / 2f + 50f, paneSize.x, paneSize.y );
+				rect.x = UI.screenWidth - rect.width - 30f;
+				var rect2 = new Rect( 0f, rect.y - 30f, UI.screenWidth - 85f, 30f );
+				var titleSize = new Vector2( 1032f, 146f );
+				var titleOverlaySize = new Vector2( 629f, 53f );
+				var titleOverlayOffset = new Vector2( -190f, 300f );
+				if( titleSize.x > UI.screenWidth ) {
+					var scaleFactor = UI.screenWidth / titleSize.x;
+					titleOverlaySize *= scaleFactor;
+					titleOverlayOffset *= scaleFactor;
+					titleSize *= scaleFactor;
+				}
+				titleOverlaySize *= 0.5f;
+				titleSize *= 0.5f;
+				titleOverlayOffset *= 0.5f;
+				GUI.DrawTexture(
+					new Rect(
+						UI.screenWidth - titleSize.x - titleOverlayOffset.x,
+						rect2.y - titleSize.y + titleOverlayOffset.y,
+						titleOverlaySize.x,
+						titleOverlaySize.y
+					),
+					Patch_MainMenuDrawer_Init.TexTitleOverlay,
+					ScaleMode.StretchToFill,
+					alphaBlend: true
+				);
 			} catch( Exception ex ) {
 				Log.Error( ex.ToString() );
 			}
